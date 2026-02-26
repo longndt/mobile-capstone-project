8ee3989b0941ad51b0afc2768221c4383674a709
Nguyen Dinh Tran Long <longndt226@gmail.com>
2026-02-26 08:15:15 +0700

    Initialize Flutter app with SignUpScreen

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'signup_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Educational App',
+      theme: ThemeData(
+        colorSchemeSeed: Colors.indigo,
+        useMaterial3: true,
+      ),
+      home: const SignUpScreen(),
+    );
+  }
+}
